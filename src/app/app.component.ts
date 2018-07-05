a69e9e45a07c6b7435d92a0fc56ea070ca0ff01b
ChakerBM <chaker707@yahoo.fr>
2018-07-05 09:07:22 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private apollo: Apollo, private httpLink: HttpLink) {
+    this.apollo.create({
+      link: httpLink.create({uri: 'http://localhost:8080/graphql'}),
+      cache: new InMemoryCache(),
+    });
+  }
+
 }
